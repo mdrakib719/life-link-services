41ef8cfca2b8e9f8a76110d7dcf7cb12f334868e
bnesa <141413773+mdrakib719@users.noreply.github.com>
2026-01-19 02:53:51 +0600

    update

---
+++ src/lib/axios.ts
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+// Create axios instance with base URL from environment variable
+const axiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+export default axiosInstance;
--- src/pages/CartPage.tsx
+++ src/pages/CartPage.tsx
@@ -179,7 +179,7 @@
 // export default PaymentDetails;
 import { useParams, useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
-import axios from "axios";
+import axios from "@/lib/axios";
 
 interface Item {
   _id: string;
